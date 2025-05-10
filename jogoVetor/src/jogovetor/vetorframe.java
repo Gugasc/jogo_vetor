17152a458ce8652d84176558ad5d98e306804550
Hugo Rios Brito <hugoba532@gmail.com>
2025-05-09 21:06:52 -0300

    - Adiciona lógica de movimentação dos botões - Adiciona verificação de jogo já iniciado - Adiciona botão para reiniciar o jogo com reinicio de tempo decorrido - Modifica 'embaralhar()' para inicializar o botão null com valor vazio "". - Corrije bugs na geração contínua de numeros aleatórios ao clicar em 'Iniciar'

--- jogoVetor/src/jogovetor/vetorframe.java
+++ jogoVetor/src/jogovetor/vetorframe.java
@@ -7,14 +7,17 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 import java.time.Instant;
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 /**
  *
- * @author gugas
+ * @author gugas, hugoriosbrito
  */
 public class vetorframe extends javax.swing.JFrame {
+    
+    //TODO: Resolver BUG que sempre reinicia o tempo decorrido se o usuário clicar no botão 'Iniciar'.
 
     /**
      * Creates new form vetorframe
@@ -22,24 +25,63 @@ public class vetorframe extends javax.swing.JFrame {
     String[] vetor = new String[16]; 
     private Instant tempoInicio;
     private Timer cronometro;
+    private JButton[] botoes;
     boolean enc = false;
+    boolean primeiraAbertura = true;
+    String tempoZerado = "00:00:00";
     Random aleatorio = new Random();
+    
+    JFrame frame = new JFrame();
+
     
     public vetorframe() {
         initComponents();
+        
+        botoes = new JButton[] {b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16};
+    }
+    
+    public void limparTela(){
+        for(int i = 0; i < 16; i++){
+            vetor[i] = ("");
+        }
+        
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        b4.setText("");
+        b5.setText("");
+        b6.setText("");
+        b7.setText("");
+        b8.setText("");
+        b9.setText("");
+        b10.setText("");
+        b11.setText("");
+        b12.setText("");
+        b13.setText("");
+        b14.setText("");
+        b15.setText("");
+        b16.setText("");
+        enc = false;
+        tempo.setText("Tempo Decorrido: " + tempoZerado);
+        
+        
     }
     
     public void embaralhar(){
+ 
         for(int i = 0; i < 15; i++){
             vetor[i] = String.valueOf(i + 1);
             System.out.println(vetor[i]);
             
         }
-        /* print do vetor organizado
+        vetor[15] = "";
+        /*
+        //print do vetor organizado
         System.out.println("Organizado: ");
         for(int i = 0; i < 16; i++){
             System.out.println(vetor[i]);
-        }*/
+        }
+        */
         
         for(int i = vetor.length - 1; i > 0; i--){
             int j = aleatorio.nextInt(i + 1);
@@ -47,38 +89,45 @@ public class vetorframe extends javax.swing.JFrame {
             vetor[i] = vetor[j];
             vetor[j] = temp;
         }
-        /* print do vetor embaralhado
+        /*
+        ///print do vetor embaralhado
         System.out.println("Embaralhado: ");
         for(int i = 0; i < 16; i++){
             System.out.println(vetor[i]);
-        }*/
+        }
+        */
     }
     
     public void iniciarJogo(){
-        
-        embaralhar();
-        
-        b1.setText(vetor[0]);
-        b2.setText(vetor[1]);
-        b3.setText(vetor[2]);
-        b4.setText(vetor[3]);
-        b5.setText(vetor[4]);
-        b6.setText(vetor[5]);
-        b7.setText(vetor[6]);
-        b8.setText(vetor[7]);
-        b9.setText(vetor[8]);
-        b10.setText(vetor[9]);
-        b11.setText(vetor[10]);
-        b12.setText(vetor[11]);
-        b13.setText(vetor[12]);
-        b14.setText(vetor[13]);
-        b15.setText(vetor[14]);
-        b16.setText(vetor[15]);
-        
-        enc = false;
+        if (primeiraAbertura == true){
+       
+            embaralhar();
+
+            b1.setText(vetor[0]);
+            b2.setText(vetor[1]);
+            b3.setText(vetor[2]);
+            b4.setText(vetor[3]);
+            b5.setText(vetor[4]);
+            b6.setText(vetor[5]);
+            b7.setText(vetor[6]);
+            b8.setText(vetor[7]);
+            b9.setText(vetor[8]);
+            b10.setText(vetor[9]);
+            b11.setText(vetor[10]);
+            b12.setText(vetor[11]);
+            b13.setText(vetor[12]);
+            b14.setText(vetor[13]);
+            b15.setText(vetor[14]);
+            b16.setText(vetor[15]);
+
+            enc = false;
+            primeiraAbertura = false;
+        } else if (primeiraAbertura == false) {
+            JOptionPane.showMessageDialog(frame,"Jogo já iniciado!", "Erro!",JOptionPane.ERROR_MESSAGE); //Adicionado verificação de jogo já iniciado
+        }
     }
     
-    public void iniciarCont(){
+    public void iniciarContador(){
         tempoInicio = Instant.now();
         cronometro = new Timer();
         
@@ -95,8 +144,20 @@ public class vetorframe extends javax.swing.JFrame {
         }, 0, 1000);
     }
     
+    public void pararContador(){
+        cronometro = new Timer();
+        
+        cronometro.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                    cronometro.cancel();
+                    tempo.setText("Tempo Decorrido: " + tempoZerado);
+            }
+        }, 0, 1000);
+    }
+    
     public String tempoDecorrido(){
-        if(tempoInicio != null){
+        if(tempoInicio != null && !enc){ //adicionado (&& !enc) para verificar também se o jogo não está encerrado
             Instant tempoAtual = Instant.now();
             Duration duracao = Duration.between(tempoInicio, tempoAtual);
             long horas = duracao.toHours();
@@ -105,9 +166,89 @@ public class vetorframe extends javax.swing.JFrame {
             return String.format("%02d:%02d:%02d", horas, minutos, segundos);
         }
         else{
-            return "00:00:00";
+            return tempoZerado;
         }
     }
+        
+    /*
+    
+    Retorna a posição int no vetor em que está o botão atual clicado, se não, retorna -1(que não causa movimentação de nenhum botão).
+    
+    */
+    public int getPosicaoBotaoAtual(String[] posicoes, String textoBotaoAtual){ //retira a posição no vetor 
+        for (int i = 0; i < posicoes.length; i++){
+            if (posicoes[i].equals(textoBotaoAtual)){
+                return i;
+            }
+        }
+        return -1;
+    };
+    
+    /*
+    
+    Retorna a posição do botão ao redor do botão atual que está vazio
+    
+    */
+    public int getProximoBotaoVazio(String[] posicoes, String textoBotaoAtual){        
+        int posAtual = getPosicaoBotaoAtual(posicoes, textoBotaoAtual);
+        int posicaoDireita = posAtual + 1;
+        int posicaoEsquerda = posAtual - 1;
+        
+        int posicaoCima = posAtual - 4;
+        int posicaoCimaDireita = posAtual - 3;
+        int posicaoCimaEsquerda = posAtual - 5;
+        
+        int posicaoBaixo = posAtual + 4;
+        int posicaoBaixoDireita = posAtual + 5;
+        int posicaoBaixoEsquerda = posAtual + 3;
+        
+        int[] aVerificar = {posicaoDireita,posicaoEsquerda,posicaoCima,posicaoCimaDireita,posicaoCimaEsquerda,posicaoBaixo,posicaoBaixoDireita,posicaoBaixoEsquerda};
+        
+        for (int i = 0; i < aVerificar.length; i++){
+            int posVizinho = aVerificar[i];
+            if (posVizinho >= 0 && posVizinho < posicoes.length){
+                if (posicoes[posVizinho] == null || posicoes[posVizinho].equals("")){
+                    return posVizinho;
+                }
+            }
+        }
+
+        return -1; 
+    };
+    
+    
+    /*
+    
+    Troca a posição dos botões 
+    
+    */
+    public void trocarValoresBotoes(String textoPosAtual){
+        int posProx = getProximoBotaoVazio(vetor, textoPosAtual);
+        int posAtual = getPosicaoBotaoAtual(vetor, textoPosAtual);
+        
+        if (!textoPosAtual.equals("")){
+            String tempTexto = vetor[posAtual];
+            vetor[posAtual] = vetor[posProx];
+            vetor[posProx]  = tempTexto;
+            
+            atualizarInterface();
+                               
+        }
+    }
+    
+    /*
+    
+    Atualiza a interface após a jogada
+    
+    */
+    
+    public void atualizarInterface(){
+        for(int i =0; i < vetor.length; i++){
+            botoes[i].setText(vetor[i]);
+        }
+        
+    }
+    
     
     /**
      * This method is called from within the constructor to initialize the form.
@@ -137,6 +278,7 @@ public class vetorframe extends javax.swing.JFrame {
         tempo = new javax.swing.JLabel();
         mensagemIni = new javax.swing.JLabel();
         iniciar = new javax.swing.JButton();
+        reiniciar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -254,7 +396,7 @@ public class vetorframe extends javax.swing.JFrame {
         getContentPane().add(b16, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 240, 80, 80));
 
         tempo.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        tempo.setText("Tempo Decorrido:");
+        tempo.setText("Tempo Decorrido: 00:00:00 ");
         getContentPane().add(tempo, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 130, -1, -1));
 
         mensagemIni.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
@@ -270,78 +412,123 @@ public class vetorframe extends javax.swing.JFrame {
         });
         getContentPane().add(iniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 90, -1, -1));
 
+        reiniciar.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        reiniciar.setText("Reiniciar");
+        reiniciar.setActionCommand("reiniciar");
+        reiniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                reiniciarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(reiniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 90, -1, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void b11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b11ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b11.getText());
     }//GEN-LAST:event_b11ActionPerformed
 
     private void b1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b1ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b1.getText());
     }//GEN-LAST:event_b1ActionPerformed
 
     private void b2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b2ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b2.getText());
+
     }//GEN-LAST:event_b2ActionPerformed
 
     private void b3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b3ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b3.getText());
+
     }//GEN-LAST:event_b3ActionPerformed
 
     private void b4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b4ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b4.getText());
+
     }//GEN-LAST:event_b4ActionPerformed
 
     private void b5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b5ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b5.getText());
+
     }//GEN-LAST:event_b5ActionPerformed
 
     private void b6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b6ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b6.getText());
+
     }//GEN-LAST:event_b6ActionPerformed
 
     private void b7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b7ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b7.getText());
     }//GEN-LAST:event_b7ActionPerformed
 
     private void b8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b8ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b8.getText());
     }//GEN-LAST:event_b8ActionPerformed
 
     private void b9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b9ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b9.getText());
     }//GEN-LAST:event_b9ActionPerformed
 
     private void b10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b10ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b10.getText());
     }//GEN-LAST:event_b10ActionPerformed
 
     private void b12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b12ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b12.getText());
     }//GEN-LAST:event_b12ActionPerformed
 
     private void b13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b13ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b13.getText());
+        
     }//GEN-LAST:event_b13ActionPerformed
 
     private void b14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b14ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b14.getText());
     }//GEN-LAST:event_b14ActionPerformed
 
     private void b15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b15ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b15.getText());
     }//GEN-LAST:event_b15ActionPerformed
 
     private void b16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b16ActionPerformed
         // TODO add your handling code here:
+        trocarValoresBotoes(b16.getText());
     }//GEN-LAST:event_b16ActionPerformed
 
     private void iniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_iniciarActionPerformed
         // TODO add your handling code here:
-        iniciarCont();
+        iniciarContador();
         iniciarJogo();
     }//GEN-LAST:event_iniciarActionPerformed
+
+    private void reiniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reiniciarActionPerformed
+        // TODO add your handling code here:
+        int resposta = JOptionPane.showConfirmDialog(frame,"Quer mesmo reiniciar?", "Reiniciar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (resposta == JOptionPane.YES_OPTION){
+            limparTela();
+            primeiraAbertura = true;
+            pararContador();
+            enc=true;
+        } else if (resposta == JOptionPane.NO_OPTION){
+            frame.dispose();
+        }
+    }//GEN-LAST:event_reiniciarActionPerformed
 
 
     /**
@@ -398,6 +585,7 @@ public class vetorframe extends javax.swing.JFrame {
     private javax.swing.JButton b9;
     private javax.swing.JButton iniciar;
     private javax.swing.JLabel mensagemIni;
+    private javax.swing.JButton reiniciar;
     private javax.swing.JLabel tempo;
     // End of variables declaration//GEN-END:variables
 }
